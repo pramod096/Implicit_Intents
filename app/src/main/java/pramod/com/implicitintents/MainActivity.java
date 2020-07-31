bb2e3e382311b7d983b50158663057f834e3c011
Pramod Reddy Gonegari <pramodreddy096@gmail.com>
2020-07-31 18:20:58 +0530

    Initial commit

---
+++ app/src/main/java/pramod/com/implicitintents/MainActivity.java
@@ -0,0 +1,22 @@
+package pramod.com.implicitintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText url;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        url = findViewById(R.id.editTextTextPersonName);
+        btn = findViewById(R.id.button2);
+    }
+}
