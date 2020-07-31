a521c20d89f95383f4f9e92ef07ba65231fc97ca
Pramod Reddy Gonegari <pramodreddy096@gmail.com>
2020-07-31 18:48:21 +0530

    Created openmaps method

--- app/src/main/java/pramod/com/implicitintents/MainActivity.java
+++ app/src/main/java/pramod/com/implicitintents/MainActivity.java
@@ -2,15 +2,21 @@ package pramod.com.implicitintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.InetAddresses;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText url;
     private Button btn;
+    private EditText maps;
+    private Button mapsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +25,35 @@ public class MainActivity extends AppCompatActivity {
 
         url = findViewById(R.id.editTextTextPersonName);
         btn = findViewById(R.id.button2);
+        maps = findViewById(R.id.editTextTextPersonName2);
+        mapsBtn = findViewById(R.id.button3);
         
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openURL();
             }
-        }); 
+        });
+
+        mapsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMaps();
+            }
+        });
+    }
+
+    private void openMaps() {
+        Intent m = new Intent();
+        m.setAction(Intent.ACTION_VIEW);
     }
 
     private void openURL() {
+        Intent i = new Intent();
+        i.setAction(Intent.ACTION_VIEW);
+        String link = url.getText().toString();
+        i.setData(Uri.parse("https://"+link));
+
+        startActivity(i);
     }
 }
